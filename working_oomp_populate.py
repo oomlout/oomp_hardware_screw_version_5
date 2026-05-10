157a152009c64ecbdd9f75085326a5c38ae51381
Oom Lout <aaron@oomlout.com>
2026-05-10 16:26:35 +0100

    changes

--- working_oomp_populate.py
+++ working_oomp_populate.py
@@ -1,4 +1,3 @@
-
 import copy
 import itertools
 
@@ -10,35 +9,30 @@ from oomp_populate_helper import write_extras
 def main(**kwargs):
     # Define default input dict with all required fields
     default_input = {
-        "taxonomy_1": "decoration",
-        "taxonomy_2": "party",
+        "taxonomy_1": "hardware",
+        "taxonomy_2": "screw",
         "taxonomy_3": "",
         "taxonomy_4": "",
         "taxonomy_5": "",
         "taxonomy_6": "",
         "taxonomy_7": "",
         "taxonomy_8": "",
-        "taxonomy_9": "",
-        "taxonomy_10": "",
         # Add any additional details here
     }
      
     
     #### define extra entries
-    #taxonomy_3 
-    #taxonomy_4 
-    #taxonomy_5 diameter
-    #taxonomy_6 depth
-    #taxonomy_7 hole_top_diameter
+    #taxonomy_3 style           countersunk, grub, machine_screw, self_tapping, socket_cap, wood    
+    #taxonomy_4 drive type      hex_head, philips, pozidriv, slotted, torx
+    #taxonomy_5 colour          black
+    #taxonomy_6 thread size     m3, m4, m5
+    #taxonomy_7 length         10mm, 20mm, 30mm
     #taxonomy_14 manufacturer
     #taxonomy_15 manufacturer_part_number
-    oobb_details_add = []
-    
     options = []
     #define single parts (take the default options add one with the extra details)
     option = {}
     
-
     ############################# examples
     #flourescent green # multiline example
     if False:        
@@ -50,73 +44,95 @@ def main(**kwargs):
         options.append(copy.deepcopy(option))
     
     #flourescent green # singleline example
-    if False:
-            # taxonomy_4 80 gsm                
+    if False:        
         options.append({"taxonomy_4": "80_gsm",       "taxonomy_5": "green_flourescent",  "taxonomy_14": "papago",    "taxonomy_15": "21403"})
-    if False:    
-        #furniture
-        if True:
-            #shelf
-            if True:
-                #ikea
-                if True:
-                    #billy
-                    options.append({"taxonomy_1": "furniture", "taxonomy_2": "shelf", "taxonomy_3": "ikea", "taxonomy_4": "billy", "taxonomy_5": "400_mm_width_1060_mm_height_280_mm_depth", "taxonomy_14": "ikea", "taxonomy_15": "802_638_32", "url_source": "https://www.ikea.com/gb/en/p/billy-bookcase-white-80263832/"})
-                    options.append({"taxonomy_1": "furniture", "taxonomy_2": "shelf", "taxonomy_3": "ikea", "taxonomy_4": "billy", "taxonomy_5": "400_mm_width_2020_mm_height_280_mm_depth", "taxonomy_14": "ikea", "taxonomy_15": "502_638_38", "url_source": "https://www.ikea.com/gb/en/p/billy-bookcase-white-50263838/"})
-                    options.append({"taxonomy_1": "furniture", "taxonomy_2": "shelf", "taxonomy_3": "ikea", "taxonomy_4": "billy", "taxonomy_5": "800_mm_width_1060_mm_height_280_mm_depth", "taxonomy_14": "ikea", "taxonomy_15": "302_638_44", "url_source": "https://www.ikea.com/gb/en/p/billy-bookcase-white-30263844/"})
-                    options.append({"taxonomy_1": "furniture", "taxonomy_2": "shelf", "taxonomy_3": "ikea", "taxonomy_4": "billy", "taxonomy_5": "800_mm_width_2020_mm_height_280_mm_depth", "taxonomy_14": "ikea", "taxonomy_15": "002_638_50", "url_source": "https://www.ikea.com/gb/en/p/billy-bookcase-white-00263850/"})
-                    options.append({"taxonomy_1": "furniture", "taxonomy_2": "shelf", "taxonomy_3": "ikea", "taxonomy_4": "billy", "taxonomy_5": "800_mm_width_2020_mm_height_400_mm_depth", "taxonomy_14": "ikea", "taxonomy_15": "904_019_32", "url_source": "https://www.ikea.com/gb/en/p/billy-bookcase-white-90401932/"})
-                    options.append({"taxonomy_1": "furniture", "taxonomy_2": "shelf", "taxonomy_3": "ikea", "taxonomy_4": "billy", "taxonomy_5": "800_mm_width_2370_mm_height_280_mm_depth", "taxonomy_14": "ikea", "taxonomy_15": "591_822_01", "url_source": "https://www.ikea.com/gb/en/p/billy-bookcase-white-s59182201/"})
-                    
-    ########################### real
-    if True:
-          
-        option["person"] = "maya"
-        option["reason"] = "birthday"
-        option["age"] = "6"
-        option["theme_1"] = "star"
-        option["theme_2"] = ""
-        option["theme_3"] = ""
-        option["theme_4"] = ""
-        option["theme_5"] = ""
-        options.append(copy.deepcopy(option))
         
-    #load from working_manual.yaml
-    if False:
-        with open("working_manual.yaml", 'r', encoding='utf-8') as file:
-            import yaml
-            data = yaml.safe_load(file)
-            options_yaml = data.get("options", [])
-            for option_yaml in options_yaml:
-                options.append(option_yaml)
 
+    #################### for this project
+    #countersunk hex head black
+    if True:        
+        current_taxonomy_3 = "countersunk"
+        current_taxonomy_4 = "hex_head"
+        current_taxonomy_5 = "black"
+        extras_sizes = {}
+        extras_sizes["m2"] = [3,5,6,8,10,12,14,16,20,22,25]
+        extras_sizes["m3"] = [4,5,6,8,10,12,16,20,25,30,35]
+        extras_sizes["m4"] = [6,8,10,12,16,20,25,30,35,40]   
+
+        for extra_size in extras_sizes:
+            for extra_size2 in extras_sizes[extra_size]:
+                option = {}
+                option["taxonomy_3"] = current_taxonomy_3
+                option["taxonomy_4"] = current_taxonomy_4
+                option["taxonomy_5"] = current_taxonomy_5
+                option["taxonomy_6"] = f"{extra_size}_diameter"
+                option["taxonomy_7"] = f"{extra_size2}_mm_length"
+                options.append(copy.deepcopy(option))
     
-    ###### populate taxonomy details and oobb details
+    #socket_cap hex head black
+    if True:        
+        current_taxonomy_3 = "socket_cap"
+        current_taxonomy_4 = "hex_head"
+        current_taxonomy_5 = "black"
+        extras_sizes = {}
+        extras_sizes["m2"] = [3,4,5,6,8,10,12,14,16,18,20,25]
+        extras_sizes["m2_5"] = [4,5,6,8,10,12,16,20,25]
+        extras_sizes["m3"] = [5,6,8,10,12,18,16,20,25,30,35,40,45,50,60]
+        extras_sizes["m4"] = [4,6,8,10,12,14,16,20,25,30,35,40,45,50,60,65,70,75]
+        extras_sizes["m5"] = [6,8,10,12,14,16,20,25,30,35,40,45,50,60,65,70,75,80,90,100,110,120]
+        extras_sizes["m6"] = [8,12,16,20,25,30,35,40,45,50,60,65,70,80,90,100]  
+
+        for extra_size in extras_sizes:
+            for extra_size2 in extras_sizes[extra_size]:
+                option = {}
+                option["taxonomy_3"] = current_taxonomy_3
+                option["taxonomy_4"] = current_taxonomy_4
+                option["taxonomy_5"] = current_taxonomy_5
+                option["taxonomy_6"] = f"{extra_size}_diameter"
+                option["taxonomy_7"] = f"{extra_size2}_mm_length"
+                #### extra specific ones
+                if True:
+                    option["thread_size"] = extra_size
+                    option["length"] = extra_size2
+                    screw_style = f"{default_input['taxonomy_2']}_{option['taxonomy_3']}"
+                    option["screw_style"] = screw_style                    
+                options.append(copy.deepcopy(option))
+
+
+    #add oobb_details
     if True:
-        for option in options:            
-            option["taxonomy_3"] = f"{option.get('reason', '')}_reason"
-            option["taxonomy_4"] = f"{option.get('age', '')}_age"
-            theme_string = ""
-            for i in range(1, 6):
-                theme_string += f"{option.get(f'theme_{i}', '')}_"
-            theme_string = theme_string.rstrip("_")          
-            option["taxonomy_5"] = f"{theme_string}_theme"
-            option["taxonomy_6"] = f"{option.get('person', '')}_person"
-            if False:
-                pass
-                oobb_details = {}
-                #taxonomy_4 hole_cover
-                oobb_details["oobb_name"] = option_type
-                oobb_details["diameter"] = option.get("diameter", None)            
-                oobb_details["depth"] = option.get("depth", None)
-                option["oobb_details"] = oobb_details
-    
+        for option in options:
+            #option = options[option_id]
+            oobb_details = {}
+            oobb_details["oobb_name"] = "screw"
+            oobb_details["thread_size"] = option.get("taxonomy_6", "default")
+            oobb_details["length"] = option.get("taxonomy_7", "default")
+            oobb_details["drive_style"] = option.get("taxonomy_4", "default")
+            oobb_details["screw_style"] = option.get("taxonomy_3", "default")
+            oobb_details["screw_colour"] = option.get("taxonomy_5", "default")
+            option["oobb_details"] = oobb_details
 
-    #load the options into full list
+    #define loop parts here
+    if False:
+        options = looping_options(default_input, options)
+
+    #define oobb parts here
+    if False:
+        option = {}
+        option["oobb"] = True
+        option["width"] = 5
+        option["height"] = 6
+        option["depth"] = 21
+        #name oobb_holder
+        option["oobb_name"] = "holder"
+        options.append(option)
+
     extras = []
     for option in options:
         extra = copy.deepcopy(default_input)
         extra.update(option)
+        
+        
         extras.append(extra)
 
 
